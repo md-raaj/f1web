30ceebf50950a9a9f9ed822a3120c5e7a578933d
Mohammad Raaj Attari <111295333+md-raaj@users.noreply.github.com>
2022-08-15 21:07:52 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// function toggleMenu() {
+//     const mobileMenuContent = document.getElementById('mobileMenuContent')
+//     if (mobileMenuContent.getAttribute('class') === 'hidden') {
+//       mobileMenuContent.setAttribute('class', 'block')
+//     } else {
+//       mobileMenuContent.setAttribute('class', 'hidden')
+//     }
+// }
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
+}
+
+
